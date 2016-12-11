827c22ac929c9df503d46b67a66690c22b067676
Alexandre Hamez <alexandre.hamez@gmail.com>
2016-12-11 18:58:56 +0100

    Remove useless mask

--- lib/varint/leb128.ex
+++ lib/varint/leb128.ex
@@ -21,7 +21,7 @@ defmodule Varint.LEB128 do
   """
   @spec encode(non_neg_integer) :: binary
   def encode(v) when v < 128, do: <<v>>
-  def encode(v)             , do: <<1::1, (v &&& 127)::7, encode(v >>> 7)::binary>>
+  def encode(v)             , do: <<1::1, v::7, encode(v >>> 7)::binary>>
 
 
   @doc """
