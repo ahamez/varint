5f73371d9bd9dba238d97900dff9840d2819861f
Alexandre Hamez <alexandre.hamez@gmail.com>
2016-12-11 10:39:03 +0100

    Doctests

--- lib/varint/zigzag.ex
+++ lib/varint/zigzag.ex
@@ -2,18 +2,26 @@ defmodule Varint.Zigzag do
 
   use Bitwise
 
-
   @doc """
     Encodes a signed integer into an unsigned integer suitable for LEB128 encoding.
 
+      iex> Varint.Zigzag.encode(0)
+      0
+
       iex> Varint.Zigzag.encode(-1)
       1
+
+      iex> Varint.Zigzag.encode(1)
+      2
 
       iex> Varint.Zigzag.encode(-2)
       3
 
       iex> Varint.Zigzag.encode(-2147483648)
       4294967295
+
+      iex> Varint.Zigzag.encode(2147483647)
+      4294967294
   """
   @spec encode(integer) :: non_neg_integer
   def encode(v) when v >= 0, do: v * 2
@@ -22,11 +30,20 @@ defmodule Varint.Zigzag do
   @doc """
     Decodes an unsigned integer into a signed integer.
 
+      iex> Varint.Zigzag.decode(0)
+      0
+
       iex> Varint.Zigzag.decode(1)
       -1
 
+      iex> Varint.Zigzag.decode(2)
+      1
+
       iex> Varint.Zigzag.decode(3)
       -2
+
+      iex> Varint.Zigzag.decode(4294967294)
+      2147483647
 
       iex> Varint.Zigzag.decode(4294967295)
       -2147483648
