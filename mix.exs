60920744f9e399fcbecccc330b02211e254b1f59
Alexandre Hamez <alexandre.hamez@gmail.com>
2025-01-27 14:01:11 +0100

    chore: v1.5.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Varint.Mixfile do
   def project do
     [
       app: :varint,
-      version: "1.5.0",
+      version: "1.5.1",
       elixir: "~> 1.14",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
